0827a5d369fb480c3ea4c3be31172bce5c65da0a
Joffrey LAGUT <joff@macbook-pro-de-joffrey.home>
2020-04-27 06:46:29 +0200

    feat(heatmap): draw all the values in the map

--- src/heatMap.js
+++ src/heatMap.js
@@ -22,6 +22,16 @@ const monthNames = [
   "December",
 ];
 
+const getFillColor = (temperature, reference) => {
+  if (temperature >= reference - 1 && temperature <= reference + 1) {
+    return "#FFFDD0";
+  } else if (temperature > reference) {
+    return temperature < reference + 3 ? "#FDD0FF" : "#D690D9";
+  } else {
+    return temperature > reference - 3 ? "#D0D2FF" : "#9093D9";
+  }
+};
+
 /**
  * Draw the x axis to the bottom and add the ticks.
  * @param {*} g container
@@ -30,6 +40,7 @@ const monthNames = [
 const drawXAxis = (g, xScale) =>
   g
     .attr("id", "x-axis")
+    .attr("class", "axis")
     .attr("transform", `translate(0,${canvas.height - margin.bottom})`)
     .call(d3.axisBottom(xScale).tickFormat(d3.format("d")).tickSizeOuter(0));
 
@@ -41,6 +52,7 @@ const drawXAxis = (g, xScale) =>
 const drawYAxis = (g, yScale) =>
   g
     .attr("id", "y-axis")
+    .attr("class", "axis")
     .attr("transform", `translate(${margin.left}, 0)`)
     .call(
       d3
@@ -87,6 +99,9 @@ const drawChart = (data) => {
     d3.max(data.monthlyVariance, (data) => data.year),
   ];
 
+  console.log(d3.min(data.monthlyVariance, (data) => data.temperature));
+  console.log(d3.max(data.monthlyVariance, (data) => data.temperature));
+
   const xScale = d3
     .scaleLinear()
     .domain(xDomain)
@@ -110,6 +125,27 @@ const drawChart = (data) => {
   );
   drawXAxis(svg.append("g"), xScale);
   drawYAxis(svg.append("g"), yScale);
+
+  const rectWidth = canvas.width / (xDomain[1] - xDomain[0]);
+  svg
+    .append("g")
+    .selectAll("rect")
+    .data(data.monthlyVariance)
+    .enter()
+    .append("rect")
+    .attr("class", "cell")
+    .attr(
+      "fill",
+      (d) =>
+        `${getFillColor(Number(d.temperature), Number(data.baseTemperature))}`
+    )
+    .attr("height", yScale.bandwidth())
+    .attr("width", (d) => rectWidth)
+    .attr("x", (d) => xScale(d.year))
+    .attr("y", (d) => yScale(d.month))
+    .attr("data-month", (d) => d.month)
+    .attr("data-year", (d) => d.year)
+    .attr("data-temp", (d) => d.temperature);
 };
 
 /**
